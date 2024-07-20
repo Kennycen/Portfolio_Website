66fc41c8efcc749f162e3028040e362cc368b7be
Kenny Cen <kennycen1976@gmail.com>
2024-07-20 08:26:50 -0400

    my first commit - commiting all files to the repository

---
+++ script.js
@@ -0,0 +1,76 @@
+let menuIcon = document.querySelector('#menu-icon');
+let navbar = document.querySelector('.navbar');
+
+menuIcon.onclick = () => {
+    menuIcon.classList.toggle('fa-xmark');
+    navbar.classList.toggle('active');
+}
+
+let sections = document.querySelector('section');
+let navLinks = document.querySelector('header nav a');
+
+window.onscroll = () => {
+    sections.forEach(sec => {
+        let top = window.scrollY;
+        let offset = sec.offsetTop - 150;
+        let height = sec.offsetHeight;
+        let id = sec.getAttribute('id');
+
+        if(top >= offset && top < offset + height) {
+            navLinks.forEach.apply(links => {
+                links.classList.remove('active');
+                document.querySelector('header nav a[href*=' + id + ']').classList.add('active');
+            });
+        };
+    });
+
+    let header = document.querySelector('header');
+    header.classList.toggle('sticky', window.scrollY > 100);
+
+    menuIcon.classList.remove('fa-xmark');
+    navbar.classList.remove('active');
+};
+
+ScrollReveal({ 
+    distance: '80px',
+    duration: 2000,
+    delay: 200,
+});
+
+ScrollReveal().reveal('.home-content, heading', { origin: 'top' });
+ScrollReveal().reveal('.home-img, .skills-container, .portfolio-box, .contact form', { origin: 'buttom' });
+ScrollReveal().reveal('.home-contact h1, .about-img', { origin: 'left' });
+ScrollReveal().reveal('.home-contact p, .about-content', { origin: 'right' });
+
+const typed = new Typed('.multiple-text', {
+    strings: ['Frontend Developer', 'Web Designer'],
+    typeSpeed: 70,
+    backSpeed: 70,
+    backDelay: 1000,
+    loop: true,
+});
+
+let tabLinks = document.getElementsByClassName('tab-links');
+let tabContents = document.getElementsByClassName('tab-contents');
+
+function openTab(tabname) {
+    // Remove active classes from all tabs and contents
+    for (let tablink of tabLinks) {
+        tablink.classList.remove('active-link');
+    }
+    for (let tabcontent of tabContents) {
+        tabcontent.classList.remove('active-tab');
+    }
+
+    // Add active class to the clicked tab and corresponding content
+    event.currentTarget.classList.add('active-link');
+    document.getElementById(tabname).classList.add('active-tab');
+}
+
+// Add event listeners to all tab links
+for (let i = 0; i < tabLinks.length; i++) {
+    tabLinks[i].addEventListener('click', function() {
+        openTab(this.getAttribute('data-tab'));
+    });
+}
+
